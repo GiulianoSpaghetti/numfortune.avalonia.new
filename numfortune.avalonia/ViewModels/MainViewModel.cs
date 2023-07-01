3c91271b99139dd4cb1446def690f21372bff7ac
Giulio Sorrentino <gsorre84@gmail.com>
2023-07-01 18:07:38 +0200

    Madonna Mia Vche Vlavoro

--- numfortune.avalonia/ViewModels/MainViewModel.cs
+++ numfortune.avalonia/ViewModels/MainViewModel.cs
@@ -1,6 +1,43 @@
-﻿namespace numfortune.avalonia.ViewModels;
+﻿using System.Net;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace numfortune.avalonia.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private static Task<HttpResponseMessage> httpResponse;
+    private static HttpClient client= new HttpClient();
+    private static Task<string> sTask;
+    private static string s;
+    public static string Tick()
+    {
+        s = "";
+        try
+        {
+            httpResponse = client.GetAsync("https://api.justyy.workers.dev/api/fortune");
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+
+        if (httpResponse.Result.IsSuccessStatusCode)
+        {
+             
+            sTask = httpResponse.Result.Content.ReadAsStringAsync();
+            s=sTask.Result;
+            s = s.Substring(1, s.Length - 2);
+            s = s.Replace("\\n", System.Environment.NewLine);
+            s = s.Replace("\\t", "	");
+            s = s.Replace("\\\"", "\"");
+        }
+        else
+        {
+            s = $"The HTTP status code is ${httpResponse.Result.StatusCode}";
+        }
+        return s;
+
+    }
 }
--- numfortune.avalonia/Views/MainView.axaml.cs
+++ numfortune.avalonia/Views/MainView.axaml.cs
@@ -2,50 +2,20 @@
 using Avalonia.Interactivity;
 using System.Net.Http;
 using System;
+using numfortune.avalonia.ViewModels;
 
 namespace numfortune.avalonia.Views;
 
 public partial class MainView : UserControl
 {
-    HttpResponseMessage httpResponse;
-    HttpClient client;
+
     public MainView()
     {
         InitializeComponent();
-        client = new HttpClient();
-        tick();
+        cookie.Text = MainViewModel.Tick();
     }
 
-    private async void tick()
-    {
-        try
-        {
-            httpResponse = await client.GetAsync("https://api.justyy.workers.dev/api/fortune");
-        }
-        catch (Exception ex)
-        {
-            cookie.Text = ex.Message;
-            return;
-        }
 
-        if (httpResponse.IsSuccessStatusCode)
-        {
-            String s = await httpResponse.Content.ReadAsStringAsync();
-            s = s.Substring(1, s.Length - 2);
-            s = s.Replace("\\n", System.Environment.NewLine);
-            s = s.Replace("\\t", "	");
-            s = s.Replace("\\\"", "\"");
-            cookie.Text = s;
-        }
-        else
-        {
-            cookie.Text = $"The HTTP status code is ${httpResponse.StatusCode}";
-        }
 
-    }
-
-    public void OnTick_Click(Object obj, RoutedEventArgs args)
-    {
-        tick();
-    }
+    public void OnTick_Click(Object obj, RoutedEventArgs args) => cookie.Text=MainViewModel.Tick();
 }
