a8f1ca51b408558e052bf4e76e96562470de1444
Giulio Sorrentino <numerone@fastwebnet.it>
2024-07-10 05:05:33 +0200

    Aggiornato ad avalonia 11.1.0-rc2

--- numfortune-setup/Program.cs
+++ numfortune-setup/Program.cs
@@ -28,9 +28,9 @@ namespace windatefrom_setup
             project.GUID = new Guid("90D24925-7418-48AD-9950-676C2F839B20");
             project.Version = new Version("2.0.3");
             project.Platform = Platform.x64;
-            project.SourceBaseDir = "F:\\source\\numfortune.avalonia.new\\numfortune.Desktop\\bin\\Release\\net8.0";
+            project.SourceBaseDir = "E:\\source\\numfortune.avalonia.new\\numfortune.Desktop\\bin\\Release\\net8.0";
             project.LicenceFile = "LICENSE.rtf";
-            project.OutDir = "f:\\";
+            project.OutDir = "E:\\";
             project.ControlPanelInfo.Manufacturer = "Giulio Sorrentino";
             project.ControlPanelInfo.Name = "numerone's fortune in avalonia";
             project.ControlPanelInfo.HelpLink = "https://github.com/numerunix/numfortune.avalonia.new/issues";
